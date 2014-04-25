b9f4ba1dc468d854e20acee9f54f087f6251efc0
Erik Aybar <erikthedeveloper@gmail.com>
2014-04-25 14:02:13 -0600

    Now utlizing fixtures. Add test: 'POST users multiple times causes error'

--- test/integration/api_resource_users_test.rb
+++ test/integration/api_resource_users_test.rb
@@ -10,11 +10,25 @@ class ApiResourceUsers < ActionDispatch::IntegrationTest
     test 'POST users creates new user' do
         post '/users',
             { user:
-                { first_name: 'Joe', email: 'joe@joejoejoe.com' }
+                users(:joe)
             }.to_json, {'Accept' => Mime::JSON}
 
         assert_equal 201, response.status
         json_body = body_to_json(response.body)
+    end
+
+    test 'POST users multiple times causes error' do
+        post '/users',
+            { user:
+                users(:joe)
+            }.to_json, {'Accept' => Mime::JSON}
+
+        post '/users',
+            { user:
+                users(:joe)
+            }.to_json, {'Accept' => Mime::JSON}
+
+        assert_not_equal 201, response.status
     end
 
 end
